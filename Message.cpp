504a89b245e691e784c4fc5a1e568fb390c32f18
youssef <youssef11atef@gmail.com>
2025-04-19 14:47:04 +0200

    Added messageID, marking message as favorite

--- Message.cpp
+++ Message.cpp
@@ -1,7 +1,9 @@
 #include "Message.h"
 #include <iostream>
 #include <vector>
+#include <set>
 Message::Message(string data, int sender, int receiver) {
+    messageid = generateRandomId();
     content = data;
     senderid = sender;
     this->receiver = receiver;
@@ -18,11 +20,31 @@ int Message::getSenderId() const { return senderid; }
 int Message::getReceiver() const { return receiver; }
 time_t Message::getDoc() const { return doc; }
 string Message::getDateInText() const { return dateintext; }
+int Message::getMessageId() const { return messageid; }
+bool Message::getIsFavorite() const { return isFavorite; }
 
 void Message::setContent(string s) { content = s; }
 void Message::deleteContent() { content = ""; }
 void Message::setSenderId(int i) { senderid = i; }
 void Message::setReceiver(int r) { receiver = r; }
+void Message::setFavorite(int messageid) {
+    this->messageid = messageid;
+    isFavorite = true; // In the main function, make a queue and have it iterate through the messages and enqueue any message that is marked as favorite
+}
+
+std::set<int> usedIDs;
+int Message::generateRandomId() {
+    srand(time(0));  
+    int randomId;
+    do {
+        randomId = rand() % 9000000 + 1000000;  // generate number from 1,000,000 to 9,999,999
+    } while (usedIDs.find(randomId) != usedIDs.end());  // check if id exists
+
+    usedIDs.insert(randomId);
+    return randomId;
+}
+
+
 #include <iostream>
 #include <list>
 #include "Message.h"
@@ -31,8 +53,7 @@ std::string Message::viewAllMessages(std::list<Message> messages) {
     std::string result;
     for (auto it = messages.rbegin(); it != messages.rend(); ++it) {
         const Message& message = *it;
-        result += "Sender ID: " + std::to_string(message.getSenderId()) + "\n";
-        result += "Receiver ID: " + std::to_string(message.getReceiver()) + "\n";
+        result += "Message ID: " + std::to_string(message.getMessageId()) + "\n";
         result += "Date: " + message.getDateInText() + "\n";
         result += "Content: " + message.getContent() + "\n";
         result += "------------------------\n";
--- Message.h
+++ Message.h
@@ -9,17 +9,20 @@ using namespace std;
 class Message {
 private:
     string content;
+    int messageid;
     int senderid;
     int receiver;
     time_t doc;
     string dateintext;
+    bool isFavorite = false;
 
 public:
     Message(string data, int sender, int receiver);
-
     string getContent() const;
     int getSenderId() const;
     int getReceiver() const;
+    int getMessageId() const;
+    bool getIsFavorite() const;
     time_t getDoc() const;
     string getDateInText() const;
 
@@ -28,6 +31,8 @@ public:
     void setSenderId(int i);
     void setReceiver(int r);
     std::string viewAllMessages(std::list<Message> messages);
+    void setFavorite(int messageid);
+    int generateRandomId();
     };
 
 #endif
